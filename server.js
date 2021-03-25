ba14c3c3f8f63a504b7918e960cf7cf2afdb4a55
daniel mestawat <dannymamusha@gmail.com>
2021-03-24 22:42:28 -0400

    update on server.js,service-worker,manifest,index.html

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
 const app = express();
+
+var PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
@@ -15,9 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex:true,
+  useUnifiedTopology: true 
 });
 
 // routes
